bfa9863a4361829cf1f4148c668b4dd3e140dec7
Ian Odhiambo <you@example.com>
2019-05-02 12:02:46 +0300

    install css framework sematic-ui, boots4 and font-awsome

---
+++ src/Services/CategoryService.js
@@ -0,0 +1,45 @@
+export const categories =[
+  {
+    "_id": "5cc986ddc2f55e8e4c6e6ba5",
+    "category": "Laptop"
+  },
+  {
+    "_id": "5cc986ddc4ecba20dd07e529",
+    "category": "Television"
+  },
+  {
+    "_id": "5cc986dd1d8c232cb9b5ea77",
+    "category": "Accessories"
+  },
+  {
+    "_id": "5cc986dd66f621f1536d1496",
+    "category": "Phone"
+  },
+  {
+    "_id": "5cc986dd5b6812fc83ca5b37",
+    "category": "Camera"
+  },
+  {
+    "_id": "5cc986dd2c4b0b9e99252494",
+    "category": "Home_Theater"
+  },
+  {
+    "_id": "5cc986dd3d3746495f5278d4",
+    "category": "Monitor"
+  },
+  {
+    "_id": "5cc986dd66366ef9d3ce1620",
+    "category": "VGA"
+  },
+  {
+    "_id": "5cc986ff66f621f1586d1446",
+    "category": "Audio"
+  },
+  {
+    "_id": "5cc986dt1d8c252cb9b5es77",
+    "category": "Desktop"
+];
+
+export function getCategories() {
+  return categories.filter(c => c);
+}
---
+++ src/Services/CompanyService.js
@@ -0,0 +1,63 @@
+export const companies = [
+ {
+    "iso": "5cc98af465323d13f25f622c",
+    "company_name": "Samsung"
+  },
+  {
+    "iso": "5cc98af4b9bfed9fe6e633cd",
+    "company_name": "Lenovo"
+  },
+  {
+    "iso": "5cc98af474d4e9881232b6e9",
+    "company_name": "LG"
+  },
+  {
+    "iso": "5cc98af47feab7457c8f17cb",
+    "company_name": "Apple"
+  },
+  {
+    "iso": "5cc98af46ecd0459d0a6cca4",
+    "company_name": "Asus"
+  },
+  {
+    "iso": "5cc98af408a5b915e3881359",
+    "company_name": "AMD"
+  },
+  {
+    "iso": "5cc98af4cc9e362b91638ffa",
+    "company_name": "Sony"
+  },
+  {
+    "iso": "5cc98af4434983902fe62651",
+    "company_name": "Dell"
+  },
+  {
+    "iso": "5cc98af4808bf421ddeecc4c",
+    "company_name": "Nvidia
+"
+  },
+	{
+    "iso": "5cc98af4808gf521ddeeec4c",
+    "company_name": "Huawei"
+  },
+	{
+    "iso": "5cc98af9008gf521ddyuec9c",
+    "company_name": "OnePlus"
+  },
+  {
+    "iso": "5cc98af0b9tfed9fe9e644cd",
+    "company_name": "Panasonic"
+  },
+  {
+    "iso": "5cc98af490323d13f25f699c",
+    "company_name": "Nikon"
+  },
+  {
+    "iso": "5cc98af07frtb7457c8f17vb",
+    "company_name": "Canon"
+  }
+];
+
+export function getCompanies() {
+  return genres.filter(company => company);
+}
