942192692b939aa2620b0bcc7107995968a7f864
Ian Odhiambo <you@example.com>
2019-05-02 21:05:08 +0300

    Implemented pagination with help of lodash for page calculation

--- src/Components/Pagination.jsx
+++ src/Components/Pagination.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React from 'react';
+import _ from 'lodash';
 
-const Pagination=(props)=> {
-  return (
-   <nav>
-       <ul className="pagination">
-        <li className="page-item">
-            
-        </li>
-       </ul>
-   </nav>
-  )
+
+
+
+
+const Pagination = (props) => {
+    const {productsCount, pageSize }= props;
+    const pagesCount = Math.ceil(productsCount / pageSize);
+
+    const pages =_.range(1, pagesCount+1);
+	return (
+		<nav>
+			<ul className="pagination">
+            {pages.map(page=> 
+				<li className="page-item">
+					<a className="page-link">{page}</a>
+				</li>            
+             )}
+			</ul>
+		</nav>
+	);
+};
+Pagination.propTypes = {
+    
 }
 
-default Pagination
+export default Pagination;
--- src/Components/Product/Products.jsx
+++ src/Components/Product/Products.jsx
@@ -3,18 +3,26 @@ import { getProducts } from '/home/renegade/Documents/My Projects/React/tech_it/
 import '../StylesCommon/responsive.css';
 import '../StylesCommon/ui.css';
 import ProductDetails from './ProductDetails';
+import Pagination from '../Pagination';
 
 class Products extends Component {
-	state = { products: [] };
+	state = {
+		products: [],
+		pageSize: 9
+	};
 
 	componentDidMount() {
 		this.setState({ products: getProducts() });
 	}
 	render() {
-		console.log(this.state.products);
+		const { length: count } = this.state.products;
+		const { products, pageSize } = this.state;
+
+		console.log(products);
 		return (
 			<div className="container m-auto">
-				<ProductDetails className="mx-auto" products={this.state.products} />
+				<ProductDetails className="mx-auto" products={products} />
+				<Pagination productsCount={count} pageSize={pageSize} />
 			</div>
 		);
 	}
