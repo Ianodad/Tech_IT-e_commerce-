3c2297434e58b8e96be873a1f1f6085af622df4c
Ian Odhiambo <you@example.com>
2019-09-04 16:51:23 +0300

    Updates

---
+++ precache-manifest.b2bb7e1016ef85a581b52f18dc79d7e9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e839950aaed35b5337f226ffdb2af7ff",
+    "url": "/Tech_IT-e_commerce-/index.html"
+  },
+  {
+    "revision": "060871c64edb31caaa1b",
+    "url": "/Tech_IT-e_commerce-/static/css/2.c5730487.chunk.css"
+  },
+  {
+    "revision": "529b626237bcc6284c3b",
+    "url": "/Tech_IT-e_commerce-/static/css/main.e5a974f4.chunk.css"
+  },
+  {
+    "revision": "060871c64edb31caaa1b",
+    "url": "/Tech_IT-e_commerce-/static/js/2.e5072b41.chunk.js"
+  },
+  {
+    "revision": "529b626237bcc6284c3b",
+    "url": "/Tech_IT-e_commerce-/static/js/main.75e700a3.chunk.js"
+  },
+  {
+    "revision": "eb4f12535d23d4d7851a",
+    "url": "/Tech_IT-e_commerce-/static/js/runtime~main.a4cc65a9.js"
+  },
+  {
+    "revision": "055dc973340fd5df7ef16c341f25d25d",
+    "url": "/Tech_IT-e_commerce-/static/media/Appliance-repair-san-antonio-65.055dc973.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Tech_IT-e_commerce-/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Tech_IT-e_commerce-/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Tech_IT-e_commerce-/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Tech_IT-e_commerce-/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Tech_IT-e_commerce-/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
